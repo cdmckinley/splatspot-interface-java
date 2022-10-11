e92b8c49ba94f0b01eca654b1d57086160ac0349
cdmckinley <work.craigmckinley@gmail.com>
2022-10-10 19:37:41 -0500

    Now clears shared_media table as well

--- src/test/resources/cleanup.sql
+++ src/test/resources/cleanup.sql
@@ -1,3 +1,4 @@
+delete from shared_media;
 delete from USER;
 INSERT INTO USER VALUES (1,'Nick','SW-1111-1111-1111','You''reIt','0123','n','y'),
                         (2,'Nicholas','SW-2222-2222-2222','NoTagBacks','4567','y','n');
