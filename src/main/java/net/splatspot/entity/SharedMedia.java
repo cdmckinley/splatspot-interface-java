f8d104946fc00b806f2aae0c33bc02f57b57f355
cdmckinley <work.craigmckinley@gmail.com>
2022-10-10 19:31:51 -0500

    Added SharedMedia entity

---
+++ src/main/java/net/splatspot/entity/SharedMedia.java
@@ -0,0 +1,49 @@
+package net.splatspot.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+// TODO JavaDocs :)
+@Entity(name = "SharedMedia")
+@Table(name = "shared_media")
+public class SharedMedia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @ManyToOne
+    private User user;
+
+    private String link;
+
+    public SharedMedia() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
