12aac2d3d4dd2e1649c0f26989495d54bb061de4
cdmckinley <work.craigmckinley@gmail.com>
2022-10-12 18:23:28 -0500

    Updated with 'shared_media' table

--- recreate-test-table.sql
+++ recreate-test-table.sql
@@ -1,23 +1,33 @@
--- -- Modified the output of MySQL dump
--- --
--- -- Table structure for table `user`
--- --
+-- Modified the output of MySQL dump
 --
--- DROP TABLE IF EXISTS `user`;
---
--- CREATE TABLE `user` (
---   `id` int NOT NULL AUTO_INCREMENT,
---   `nickname` varchar(25) DEFAULT NULL,
---   `friend_code` varchar(17) DEFAULT NULL,
---   `splash_tag_name` varchar(10) DEFAULT NULL,
---   `splash_tag_number` varchar(4) DEFAULT NULL,
---   `share_info_with_users` char(1) NOT NULL,
---   `share_when_ready_to_play` char(1) NOT NULL,
---   PRIMARY KEY (`id`)
--- );
---
--- -- Insert test data
--- INSERT INTO USER VALUES (1,'Nick','SW-1111-1111-1111','You''reIt','0123','n','y'),
---                         (2,'Nicholas','SW-2222-2222-2222','NoTagBacks','4567','y','n');
+-- Drop tables
+DROP TABLE IF EXISTS `shared_media`;
+DROP TABLE IF EXISTS `user`;
 
--- TODO update to include the one to many information
+-- Create tables
+
+CREATE TABLE `user` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `nickname` varchar(25) DEFAULT NULL,
+    `friend_code` varchar(17) DEFAULT NULL,
+    `splash_tag_name` varchar(10) DEFAULT NULL,
+    `splash_tag_number` varchar(4) DEFAULT NULL,
+    `share_info_with_users` char(1) NOT NULL,
+    `share_when_ready_to_play` char(1) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `shared_media` (
+    `user_id` int NOT NULL,
+    `link` varchar(100) NOT NULL,
+    `id` int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`id`),
+    KEY `shared_media_user_null_fk` (`user_id`),
+    CONSTRAINT `shared_media_user_null_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+)
+
+-- Insert test data
+INSERT INTO USER VALUES (1,'Nick','SW-1111-1111-1111','You''reIt','0123','n','y'),
+                        (2,'Nicholas','SW-2222-2222-2222','NoTagBacks','4567','y','n');
+
+-- TODO add test data for shared_media table
