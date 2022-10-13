8a3d60249eeae02c3f840cd3a21f9943cb79bd30
cdmckinley <work.craigmckinley@gmail.com>
2022-10-12 19:47:53 -0500

    Changed capitalization of 'user'. Added 'shared_media' test data

--- src/test/resources/cleanup.sql
+++ src/test/resources/cleanup.sql
@@ -1,4 +1,7 @@
 delete from shared_media;
-delete from USER;
-INSERT INTO USER VALUES (1,'Nick','SW-1111-1111-1111','You''reIt','0123','n','y'),
+delete from user;
+INSERT INTO user VALUES (1,'Nick','SW-1111-1111-1111','You''reIt','0123','n','y'),
                         (2,'Nicholas','SW-2222-2222-2222','NoTagBacks','4567','y','n');
+INSERT INTO shared_media VALUES (1, 'https://twitter.com/SlackHQ', 45),
+                                (1, 'https://twitter.com/discord', 46),
+                                (2, 'https://twitter.com/SplatoonNA', 47);
