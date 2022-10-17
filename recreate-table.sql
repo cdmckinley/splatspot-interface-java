f2844ff9eeb0c24b5ff857750d1abf797e6ce5d5
cdmckinley <work.craigmckinley@gmail.com>
2022-10-17 12:56:22 -0500

    Fixed indentation. Added description property for 'shared_media' table. Added semicolon to end.

--- recreate-table.sql
+++ recreate-table.sql
@@ -7,21 +7,22 @@ DROP TABLE IF EXISTS `user`;
 -- Create tables
 
 CREATE TABLE `user` (
-                        `id` int NOT NULL AUTO_INCREMENT,
-                        `nickname` varchar(25) DEFAULT NULL,
-                        `friend_code` varchar(17) DEFAULT NULL,
-                        `splash_tag_name` varchar(10) DEFAULT NULL,
-                        `splash_tag_number` varchar(4) DEFAULT NULL,
-                        `share_info_with_users` char(1) NOT NULL,
-                        `share_when_ready_to_play` char(1) NOT NULL,
-                        PRIMARY KEY (`id`)
+    `id` int NOT NULL AUTO_INCREMENT,
+    `nickname` varchar(25) DEFAULT NULL,
+    `friend_code` varchar(17) DEFAULT NULL,
+    `splash_tag_name` varchar(10) DEFAULT NULL,
+    `splash_tag_number` varchar(4) DEFAULT NULL,
+    `share_info_with_users` char(1) NOT NULL,
+    `share_when_ready_to_play` char(1) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `shared_media` (
     `user_id` int NOT NULL,
     `link` varchar(100) NOT NULL,
     `id` int NOT NULL AUTO_INCREMENT,
+    `description` varchar(500) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `shared_media_user_null_fk` (`user_id`),
     CONSTRAINT `shared_media_user_null_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-)
+);
