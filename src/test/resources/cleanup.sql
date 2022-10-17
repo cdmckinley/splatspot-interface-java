68e242391fbf152bd72ba51d76cea2bc280c306a
cdmckinley <work.craigmckinley@gmail.com>
2022-10-17 12:55:03 -0500

    Added a new test record to test a new field. Set the field for the older records to null.

--- src/test/resources/cleanup.sql
+++ src/test/resources/cleanup.sql
@@ -2,6 +2,8 @@ delete from shared_media;
 delete from user;
 INSERT INTO user VALUES (1,'Nick','SW-1111-1111-1111','You''reIt','0123','n','y'),
                         (2,'Nicholas','SW-2222-2222-2222','NoTagBacks','4567','y','n');
-INSERT INTO shared_media VALUES (1, 'https://twitter.com/SlackHQ', 45),
-                                (1, 'https://twitter.com/discord', 46),
-                                (2, 'https://twitter.com/SplatoonNA', 47);
+INSERT INTO shared_media VALUES (1, 'https://twitter.com/SlackHQ', 45, null),
+                                (1, 'https://twitter.com/discord', 46, null),
+                                (2, 'https://twitter.com/SplatoonNA', 47, null),
+    (2, 'https://twitter.com/SplatoonNA/status/1578369634981609477?s=20&t=MzavfhEJlDwBa03jAGM-Zg',
+                                    50, 'Gotta catch ''em all, amiright?');
