081d03336bdff29013b99aa723daaa0a9b122a25
cdmckinley <work.craigmckinley@gmail.com>
2022-10-23 15:29:07 -0500

    Added and modified the provided Login controller. Changed its url to be lowercase

---
+++ src/main/java/edu/matc/controller/LogIn.java
@@ -0,0 +1,57 @@
+package edu.matc.controller;
+
+import edu.matc.utilities.PropertiesLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
+
+@WebServlet(
+        urlPatterns = {"/login"}
+)
+
+/** Begins the authentication process using AWS Cognito
+ *
+ */
+public class LogIn extends HttpServlet implements PropertiesLoader {
+    Properties properties;
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    public static String CLIENT_ID;
+    public static String LOGIN_URL;
+    public static String REDIRECT_URL;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try {
+            properties = loadProperties("/cognito.properties");
+            CLIENT_ID = properties.getProperty("client.id");
+            LOGIN_URL = properties.getProperty("loginURL");
+            REDIRECT_URL = properties.getProperty("redirectURL");
+        } catch (Exception exception) {
+            logger.error("An " + exception.getClass().getName() +
+                            " occurred. Make sure a properly formatted 'cognito.properties' file exists and is readable",
+                    exception);
+        }
+    }
+
+    /**
+     * Route to the aws-hosted cognito login page.
+     * @param req servlet request
+     * @param resp servlet response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO if properties weren't loaded properly, route to an error page
+        String url = LOGIN_URL + "?response_type=code&client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URL;
+        resp.sendRedirect(url);
+    }
+}
