b8cfbc87cd86825bf9f5b990ef6b18937dc65bc0
cdmckinley <work.craigmckinley@gmail.com>
2022-10-25 16:18:07 -0500

    Added controller for Error page, with session support

---
+++ src/main/java/net/splatspot/controller/Error.java
@@ -0,0 +1,36 @@
+package net.splatspot.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = {"/error"}
+)
+public class Error extends HttpServlet {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session= req.getSession(false);
+        try {
+            String userName = session.getAttribute("userName").toString();
+            req.setAttribute("userName", userName);
+        } catch (NullPointerException npe) {
+            logger.debug("No username was found");
+        } finally {
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/error.jsp");
+            dispatcher.forward(req, resp);
+        }
+    }
+}
