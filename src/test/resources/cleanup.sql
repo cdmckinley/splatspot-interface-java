f03d8016b63d7165d6ff364a8261d3cdb3ee611b
cdmckinley <work.craigmckinley@gmail.com>
2022-10-25 14:14:41 -0500

    Added new fields to test data

--- src/test/resources/cleanup.sql
+++ src/test/resources/cleanup.sql
@@ -1,7 +1,7 @@
 delete from shared_media;
 delete from user;
-INSERT INTO user VALUES (1,'Nick','SW-1111-1111-1111','You''reIt','0123','n','y'),
-                        (2,'Nicholas','SW-2222-2222-2222','NoTagBacks','4567','y','n');
+INSERT INTO user VALUES (1,'test_nick',null,'Nick','SW-1111-1111-1111','You''reIt','0123','n','y'),
+                        (2,'test_nicholas',null,'Nicholas','SW-2222-2222-2222','NoTagBacks','4567','y','n');
 INSERT INTO shared_media VALUES (1, 'https://twitter.com/SlackHQ', 45, null),
                                 (1, 'https://twitter.com/discord', 46, null),
                                 (2, 'https://twitter.com/SplatoonNA', 47, null),
