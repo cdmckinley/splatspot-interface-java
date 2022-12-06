c01e6ae91abf4dc5450e79b755afbc6dd9b24db8
cdmckinley <work.craigmckinley@gmail.com>
2022-12-05 19:18:50 -0600

    Removed unnecessary retrieval and storage of session data

--- src/main/java/net/splatspot/controller/Error.java
+++ src/main/java/net/splatspot/controller/Error.java
@@ -9,9 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Objects;
 
 /**
  * The controller for the Error page.
@@ -35,14 +33,7 @@ public class Error extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        Object userNameAttribute = session.getAttribute("userName");
-        if (!Objects.isNull(userNameAttribute)) {
-            String userName = userNameAttribute.toString();
-            req.setAttribute("userName", userName);
-        } else {
-            logger.debug("No username was found");
-        }
+
         String errorType = req.getParameter("type");
         if (errorType.equals("not-found") || errorType.equals("server")) {
             req.setAttribute("errorType", errorType);
--- src/main/java/net/splatspot/controller/Home.java
+++ src/main/java/net/splatspot/controller/Home.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.*;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Objects;
 
 /**
  * The controller for the home page.
@@ -35,14 +33,7 @@ public class Home extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        Object userNameAttribute = session.getAttribute("userName");
-        if (!Objects.isNull(userNameAttribute)) {
-            String userName = userNameAttribute.toString();
-            req.setAttribute("userName", userName);
-        } else {
-            logger.debug("No username was found");
-        }
+
         RequestDispatcher dispatcher = req.getRequestDispatcher("/home.jsp");
         dispatcher.forward(req, resp);
     }
