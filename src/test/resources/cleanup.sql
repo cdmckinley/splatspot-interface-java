0aae0dcf7f9c17a1555534db2908b176fe80cfb6
cdmckinley <work.craigmckinley@gmail.com>
2022-12-05 18:48:58 -0600

    Removed unnecessary fields from user

--- src/test/resources/cleanup.sql
+++ src/test/resources/cleanup.sql
@@ -1,7 +1,7 @@
 delete from shared_media;
 delete from user;
-INSERT INTO user VALUES (1,'test_nick',null,'Nick','SW-1111-1111-1111','You''reIt','0123','n','y'),
-                        (2,'test_nicholas',null,'Nicholas','SW-2222-2222-2222','NoTagBacks','4567','y','n');
+INSERT INTO user VALUES (1,'test_1'),
+                        (2,'test_2');
 INSERT INTO shared_media VALUES (1, '2Rm4T2K4XZI', 45, null),
                                 (1, 'L1s3NnhEK0g', 46, null),
                                 (2, 'cJQRBlnoKHE', 47, null),
