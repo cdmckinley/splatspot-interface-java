ec0ae4900e7bf20fa7a860c7eb77033fc91392bb
cdmckinley <work.craigmckinley@gmail.com>
2022-12-10 13:19:26 -0600

    Can now log out using 'logout' query parameter, or logout link on header

--- src/main/java/net/splatspot/controller/Home.java
+++ src/main/java/net/splatspot/controller/Home.java
@@ -1,5 +1,6 @@
 package net.splatspot.controller;
 
+import net.splatspot.utilities.ServletUtilities;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.*;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -27,14 +29,23 @@ public class Home extends HttpServlet {
     /**
      * Forward to the 'home' jsp when a GET request is received
      * @param req The HttpServletRequest
-     * @param resp The HttpServletResponse
+     * @param res The HttpServletResponse
      * @throws ServletException
      * @throws IOException
      */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        String logout = req.getParameter("logout");
+
+        if (logout != null) {
+            HttpSession session = req.getSession();
+            session.invalidate();
+            ServletUtilities.redirectToPage(req, res, "/home", "/home");
+            return;
+        }
 
         RequestDispatcher dispatcher = req.getRequestDispatcher("/home.jsp");
-        dispatcher.forward(req, resp);
+        dispatcher.forward(req, res);
     }
 }
